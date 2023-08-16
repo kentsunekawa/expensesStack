4d7623643aade9dfb11626a7a949c054a14b6d1a
kentsunekawa <k.tsunekawa.dev@gmail.com>
2023-08-16 23:27:46 +0900

    CategoryBox のテスト実装

--- jest.config.ts
+++ jest.config.ts
@@ -4,9 +4,9 @@ module.exports = {
   roots: ['<rootDir>/'],
   modulePaths: ['<rootDir>'],
   moduleDirectories: ['node_modules'],
-  setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
   testEnvironment: 'jsdom',
   testEnvironmentOptions: {
     customExportConditions: ['react-native'],
   },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 }
--- src/components/parts/CategoryBox/__tests__/index.test.tsx
+++ src/components/parts/CategoryBox/__tests__/index.test.tsx
@@ -6,6 +6,18 @@ describe('CategoryBox', () => {
   it('テキストが正しく表示される', () => {
     const name = 'category name'
     render(<CategoryBox name={name} />)
-    expect(screen.getByText(name))
+    screen.getByText(name)
+  })
+  it('背景色が正しく設定される', () => {
+    const color = '#ddd'
+    const { container } = render(<CategoryBox name='hogehoge' color={color} />)
+    expect(container.querySelector('span')).toHaveStyle(
+      `background: '${color}'`,
+    )
+  })
+
+  it('Snapshot', () => {
+    const { container } = render(<CategoryBox name='hogehoge' color={'#f00'} />)
+    expect(container).toMatchSnapshot()
   })
 })
